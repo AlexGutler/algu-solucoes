0dc44f134cebec1855af66b3c45ecf047ac9827e
Alex Gutler <alex11jgt@hotmail.com>
2014-08-15 18:53:54 -0300

    criei a estrutura basica de pastas

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title> Algu Sistemas - Curso PHP </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.5">
+
+    <!-- favicon
+    <link rel="icon" type="image/x-icon" href="imagens/icons/favicon.ico"> -->
+
+    <!-- Bootstrap & CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+    <!-- Javascript -->
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <!--[if lt IE 9]>
+        <script src="js/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+</body>
+</html>
