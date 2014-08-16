cca2dceeefd494133145e12d075df27ff1cf718c
Alex Gutler <alex11jgt@hotmail.com>
2014-08-15 23:52:05 -0300

    iniciando a constrção do menu do site

---
+++ footer.php
@@ -0,0 +1,17 @@
+
+
+<section class="container rodape">
+    <footer>
+        <section></section>
+        <p>Todos os direitos reservados -
+            <?
+                date_default_timezone_set('UTC');
+                echo date("Y");
+            ?>
+        </p>
+    </footer>
+</section>
+
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title> Algu Sistemas - Curso PHP Code-Education </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.5">
+
+    <!-- favicon
+    <link rel="icon" type="image/x-icon" href="imagens/icons/favicon.ico"> -->
+
+    <!-- Bootstrap & CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+    <!-- Javascript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<header class="topo">
+
+    <div class="navbar-wrapper">
+        <div class="container-fluid">
+            <nav class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+                <div class="container">
+
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Algu Sistemas</a>
+                    </div>
+
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#">Home</a></li>
+                            <li><a href="#">Empresa</a></li>
+                            <li><a href="#">Produtos</a></li>
+                            <li><a href="#">Serviços</a></li>
+                            <li><a href="#">Contato</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </nav>
+        </div>
+    </div>
+</header>
+<hr/>
+<? require_once('footer.php') ?>
