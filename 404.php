efe3475588e37014e4fec7a4848c84b9261e0008
AlexGutler <alex11jgt@hotmail.com>
2014-08-18 14:20:05 -0300

    criei a págia 404

--- 404.php
+++ 404.php
@@ -1,3 +1,14 @@
 <? require_once('header.php') ?>
-<h1>Essa página não existe. Navegue pelos nossos menus.</h1>
+    <div class="container-404">
+        <div class="container-mensagem-404">
+            <div class="container">
+                <h1>Essa página não existe. Navegue pelos nossos menus.</h1>
+            </div>
+        </div>
+
+        <div class="container">
+            <img class="img-responsive" src="imagens/outras/404.jpg" alt="404" title="Página não encontrada!"/>
+        </div>
+
+    </div>
 <? require_once('footer.php') ?>
